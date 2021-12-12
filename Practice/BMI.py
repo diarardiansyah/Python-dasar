174eb4566ce9fb7a1f05faa4b31ed8eb0373c11c
diarardiansyah <ardiansyahdiar@gmail.com>
2021-12-12 20:00:17 +0700

    add new logic in file BMI.py

--- Practice/BMI.py
+++ Practice/BMI.py
@@ -4,6 +4,7 @@
 # Masukan inputan user tinggi badan
 print("=====Perhitungan berat badan ideal untuk laki-laki=====")
 nama = input("Masukan nama anda = ")
+beratBadan1 = float(input("Masukan berat badan anda = "))
 tinggiBadan = float(input("Masukan tinggi badan kamu = "))
 
 hasil = tinggiBadan - 100
@@ -12,10 +13,16 @@ result = hasil - hasil2
 
 print(f"Berat badan ideal {nama} adalah = {result}\n")
 
+if beratBadan1 > result:
+    print(f"{nama}berat badan kamu tidak ideal")
+else:
+    print(f"{nama}berat badan kamu ideal")
+
 # Kalulator BMI untuk perempuan
 ## masukan inputan user tinggi badan
 print("=====Perhitungan berat badan ideal untuk perempuan=====")
 nama1 = input("Masukan nama anda = ")
+beratBadan2 = float(input("Masukan berat badan anda = "))
 tinggiBadan1 = float(input("Masukan tinggi badan kamu = "))
 
 x = tinggiBadan1 - 100
@@ -24,6 +31,10 @@ h = x - y
 
 print(f"berat badan ideal {nama1} adalah {h}")
 
+if beratBadan2 > h:
+    print(f"{nama1} berat badan kamu tidak ideal")
+else:
+    print(f"{nama1} berat badan kamu ideal")
 
 
 
